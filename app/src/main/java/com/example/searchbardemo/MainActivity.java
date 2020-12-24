95089ee9d7e7499aa23c0b0a894dc31ae9a2345b
noname-developer <denchic150@mail.ru>
2020-12-24 23:04:35 +0300

    Update

--- app/src/main/java/com/example/searchbardemo/MainActivity.java
+++ app/src/main/java/com/example/searchbardemo/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.searchbar.SearchBar;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
